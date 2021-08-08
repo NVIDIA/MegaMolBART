05dca6c740707f524e02dbc07f3cc829f0430ad0
Michelle Gill <mgill@nvidia.com>
2021-08-08 13:52:13 -0700

    More cleanup

--- examples/chem/draco-rno/megamolbart_pretrain_slurm.sh
+++ examples/chem/draco-rno/megamolbart_pretrain_slurm.sh
@@ -16,9 +16,7 @@
 set -x
 
 ### CONFIG ###
-DATA_FILES_SELECTED="x_OP_000..031_CL_.csv"
-NUM_VAL_WORKERS=0
-NUM_TRAIN_WORKERS=0
+DATA_FILES_SELECTED="x_OP_000..146_CL_.csv"
 
 CONTAINER="nvcr.io#nvidian/clara-lifesciences/megamolbart_training_nemo:210716"
 STORAGE_DIR="/gpfs/fs1/projects/ent_joc/users/mgill/megatron"
@@ -43,7 +41,7 @@ MOUNTS="$CODE_DIR:$CODE_MOUNT,$OUTPUT_DIR:$OUTPUT_MOUNT,$DATA_DIR:$DATA_MOUNT"
 OUTFILE="${RESULTS_DIR}/slurm-%j-%n.out" # Can't be used with pty in srun
 ERRFILE="${RESULTS_DIR}/error-%j-%n.out"
 
-GPU_LIMIT="$(($SLURM_NTASKS-1))"
+GPU_LIMIT="$(($SLURM_GPUS_PER_NODE-1))"
 SCRIPT_CUDA_VISIBLE_DEVICES=$(seq --separator=',' 0 $GPU_LIMIT)
 SCRIPT_PYTHONPATH=${CODE_MOUNT}':$PYTHONPATH'
 
@@ -64,10 +62,8 @@ echo '*******STARTING********' \
     tokenizer.vocab_path=${CODE_MOUNT}/nemo/collections/chem/vocab/megamolbart_pretrain_vocab.txt \
     model.train_ds.filepath=${DATA_MOUNT}/train/${DATA_FILES_SELECTED} \
     model.train_ds.metadata_path=${DATA_MOUNT}/train/metadata.txt \
-    model.train_ds.num_workers=${NUM_TRAIN_WORKERS} \
     model.validation_ds.filepath=${DATA_MOUNT}/val/${DATA_FILES_SELECTED} \
     model.validation_ds.metadata_path=${DATA_MOUNT}/train/metadata.txt \
-    model.validation_ds.num_workers=${NUM_VAL_WORKERS} \
     exp_manager.wandb_logger_kwargs.name=${EXPNAME}_nodes_${SLURM_JOB_NUM_NODES}_gpus_${SLURM_GPUS_PER_NODE} \
     exp_manager.wandb_logger_kwargs.project=${PROJECT} \
     exp_manager.exp_dir=${OUTPUT_MOUNT}/${EXPNAME}_nodes_${SLURM_JOB_NUM_NODES}_gpus_${SLURM_GPUS_PER_NODE}
