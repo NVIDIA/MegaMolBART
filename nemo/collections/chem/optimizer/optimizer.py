Untested fix for optimizer global batch scaling

nemo/collections/chem/optimizer/optimizer.py
@@ -1,16 +1,17 @@
 from dataclasses import dataclass
-from typing import Tuple, Optional, Any
+from typing import Tuple, Optional, Any, Union
 from torch.optim.lr_scheduler import LambdaLR
 from dataclasses import dataclass
 from nemo.core.config import SchedulerParams
 from nemo.core.config.modelPT import OptimConfig
+from apex.transformer import parallel_state
 
 
 __all__ = ["AdamOptimConfig", "TransformerLR", "TransformerLRParams"]
 
 @dataclass
 class AdamOptimConfig(OptimConfig):
-    lr: float = 1.0 # TODO this is what was in the paper, I believe it is wrong
+    lr: float = 1.0 # TODO check this for finetuning
     betas: Tuple[float, float] = (0.9, 0.999)
     weight_decay: float = 0.0
     sched: Optional[Any]
@@ -18,17 +19,25 @@ class AdamOptimConfig(OptimConfig):
 
 @dataclass
 class TransformerLRParams(SchedulerParams):
-    lr: float = AdamOptimConfig.lr
-    warm_up_steps: int = 0
+    lr: float = 1.0
+    warm_up_steps: Optional[int] = None
     d_model: int = 256 # TODO how to automatically set to MegatronBARTConfig.d_model
     verbose: bool = False
+    micro_batch_size: int = 1
 
 
 class TransformerLR(LambdaLR):
     def __init__(self, optimizer, lr, warm_up_steps, d_model, 
-                 last_epoch: int = -1, verbose: bool = False, **kwargs):
+                 last_epoch: int = -1, verbose: bool = False, micro_batch_size: int = 1, **kwargs):
+        chemformer_global_batch_size = 4 * 128 # data_parallel_size * micro_batch_size
+        scale_factor = chemformer_global_batch_size / (parallel_state.get_data_parallel_world_size() * micro_batch_size)
+
         self.lr = lr
-        self.warm_up_steps = warm_up_steps
+        if warm_up_steps:
+            self.warm_up_steps = warm_up_steps
+        else:
+            self.warm_up_steps = int( 8000 * scale_factor ) # Scale based on Chemformer paper
+
         self.d_model = d_model
         super(TransformerLR, self).__init__(optimizer, 
                                      lr_lambda=self._transformer_lr_lambda, 