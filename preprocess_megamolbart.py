600c46a200804edf93a2a4f49bb9fe4d1016afdf
Neha Tadimeti <ntadimeti@nvidia.com>
2022-04-08 14:33:08 -0400

    Parallelize preprocessing

--- preprocess_megamolbart.py
+++ preprocess_megamolbart.py
@@ -1,6 +1,7 @@
 
 "Preprocess megamolbart data"
 "Take text files as input and output tokenized data in binarized format"
+##TODO Merge it with the CSV format data preprocessing script
 
 import argparse
 import gzip
@@ -19,7 +20,8 @@ from nemo_chem.tokenizer import MolEncTokenizer, MolEncTokenizerFromVocabFileCon
 from nemo.collections.nlp.data.language_modeling.megatron import indexed_dataset
 from rdkit import Chem
 from pysmilesutils.augment import SMILESAugmenter
-
+from functools import partial
+import multiprocessing
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -56,19 +58,19 @@ def parse_args():
     assert os.path.exists(args.input)
     return args
 
-def process_data(line, tokenizer, dataset_builder, enumerations):
-
+def process_data(line, enumerations):
     # First column is zincID and second column is the smiles string
-    _, smiles = line.strip().split(",")
-
     all_smiles = []
-    all_smiles.append(smiles)
+    zinc_id, smiles = line.strip().split(",")
+    #Ignore header
+    if (zinc_id == "zinc_id") and (smiles == "smiles"): return all_smiles
     #canonicalize the smile string
     mol = Chem.MolFromSmiles(smiles)
     canon_smiles = Chem.MolToSmiles(mol, canonical=True)
-
+    all_smiles.append(canon_smiles)
     atom_order: List[int] = list(range(mol.GetNumAtoms()))
     while(enumerations):
+        print ("Enumerations!")
         np.random.shuffle(atom_order)
         aug_mol = Chem.RenumberAtoms(mol, atom_order)
         try:
@@ -79,10 +81,7 @@ def process_data(line, tokenizer, dataset_builder, enumerations):
             ## If RDKit couldn't generate augmented smile, we ignore and try again
             pass
         enumerations = enumerations-1
-    token_output = tokenizer.tokenize(all_smiles, pad=True)
-    enc_token_ids = tokenizer.convert_tokens_to_ids(token_output["original_tokens"])
-    enc_token_ids = torch.tensor(enc_token_ids)
-    dataset_builder.add_item(enc_token_ids)
+    return all_smiles
 
 def main():
     args = parse_args()
@@ -103,7 +102,7 @@ def main():
     #merge default config with the user input config.
     cfg = OmegaConf.load(args.config)
     default_tokenizer = OmegaConf.create(MolEncTokenizerFromVocabFileConfig())
-    cfg_tokenizer = cfg.tokenizer if cfg.get('tokenizer', False) else deafult_tokenizer
+    cfg_tokenizer = cfg.tokenizer if cfg.get('tokenizer', False) else default_tokenizer
     merge_cfg_tokenizer = OmegaConf.merge(default_tokenizer, cfg_tokenizer)
     if not os.path.exists(merge_cfg_tokenizer.vocab_path):
         raise ValueError(f'Vocab file not found at {merge_cfg_tokenizer.vocab_path}')
@@ -114,26 +113,27 @@ def main():
     # Initialize a dataset writer
     os.makedirs(args.out_dir, exist_ok = True)
     output_file = os.path.join(args.out_dir, "data.bin")
-    index_file = os.path.join(args.out_dir, "index.bin")
+    index_file = os.path.join(args.out_dir, "data.idx")
 
     # We do not overwrite the existing data
     if (os.path.exists(output_file) or os.path.exists(index_file)):
         raise FileExistsError(f'The output file at the location {output_file} and {index_file}')
-    dataset_builder = indexed_dataset.make_builder(output_file, impl="mmap", vocab_size=tokenizer.vocab_size())
-
+    dataset_builder = indexed_dataset.make_builder(output_file, impl="mmap", vocab_size=tokenizer.vocab_size)
+    partial_process_func = partial(process_data, enumerations=args.enumerations)
     # Write the binarized output
     sizes = []
     batched_smiles = []
+    multiprocessing.set_start_method('spawn')
+    pool = multiprocessing.Pool(args.num_workers)
     for inputfile in inputfiles:
-        with open(inputfile, "r") as ifile:
-            # ignore the header. we assume two columns.
-            line = ifile.readline().strip().split(",")
-            assert len(line) == 2, f'The input file {inputfile} has {len(line)} columns, expected 2'
-            while (True):
-                line = ifile.readline()
-                if (not line) break 
-                process_data(line, tokenizer, dataset_builder, args.enumerations)
-            dataset_builder.end_document()
+        ifile = open(inputfile, "r")
+        out_iterator = pool.imap(partial_process_func, ifile, 25)
+        for smiles in out_iterator:
+            token_output = tokenizer.tokenize(smiles, pad=False)
+            enc_token_ids = tokenizer.convert_tokens_to_ids(token_output["original_tokens"])
+            enc_token_ids = torch.tensor(enc_token_ids)
+            dataset_builder.add_item(enc_token_ids)
+        dataset_builder.end_document()
 
     dataset_builder.finalize(index_file)
    
