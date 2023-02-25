535be462e0a52ce2d0aa63be56dbe8809eaef072
Rajesh K Ilango <rilango@nvidia.com>
2023-02-24 17:37:14 -0800

    Change to address security vulnarabilities reported by AWS.

--- setup/startup.sh
+++ setup/startup.sh
@@ -4,7 +4,4 @@ jupyter lab \
     --port=8888 \
     --ip=0.0.0.0 \
     --allow-root \
-    --notebook-dir=/opt/nvidia/nemo_chem/examples/chem/nbs \
-    --NotebookApp.password='' \
-    --NotebookApp.token='' \
-    --NotebookApp.password_required=False
+    --notebook-dir=/opt/nvidia/nemo_chem/examples/chem/nbs
